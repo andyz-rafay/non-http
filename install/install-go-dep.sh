ade162586d2aee1d862247ffb5c0c8ba57d9eaa1
Andy Zhou <azhou@rafay.co>
2019-02-19 14:14:03 -0800

    add install-go-dep.sh

--- install/install-go-dep.sh
+++ install/install-go-dep.sh
@@ -1,3 +1,1 @@
-export DEP_RELEASE_TAG=v0.4.1
-export INSTALL_DIRECTORY=/usr/local/bin
 curl https://raw.githubusercontent.com/golang/dep/master/install.sh | DEP_RELEASE_TAG=v0.4.1 INSTALL_DIRECTORY=/usr/local/bin sudo -E sh
